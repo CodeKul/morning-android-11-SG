8ef71c7c70827af4a289886f7b6fa31a81cbf3a3
aniruddha <aniruddha.kudalkar@gmail.com>
2017-12-19 12:26:53 +0530

    views and listeners

--- sample/src/main/java/com/codekul/sample/MainActivity.java
+++ sample/src/main/java/com/codekul/sample/MainActivity.java
@@ -2,6 +2,10 @@ package com.codekul.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onCar(View view) {
+        Toast.makeText(this, "Car", Toast.LENGTH_SHORT).show();
+        ImageView img = findViewById(R.id.imgVeh);
+        img.setImageResource(R.drawable.ic_directions_bike_black_24dp);
+    }
+
+    public void onPlane(View view) {
+        Toast.makeText(this, "Plane", Toast.LENGTH_SHORT).show();
+        ImageView img = findViewById(R.id.imgVeh);
+        img.setImageResource(R.drawable.ic_chopper);
     }
 }

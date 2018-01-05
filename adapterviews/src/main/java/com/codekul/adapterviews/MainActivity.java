d81204f51b87cc74ce4acbad3ef6558063183993
aniruddha <aniruddha.kudalkar@gmail.com>
2018-01-05 11:51:48 +0530

    view animations

--- adapterviews/src/main/java/com/codekul/adapterviews/MainActivity.java
+++ adapterviews/src/main/java/com/codekul/adapterviews/MainActivity.java
@@ -2,7 +2,9 @@ package com.codekul.adapterviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -78,5 +80,11 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lstView = findViewById(R.id.lstVw);
         lstView.setAdapter(adapter);
+        lstView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
     }
 }
---
+++ viewanimations/src/main/java/com/codekul/viewanimations/MainActivity.java
@@ -0,0 +1,26 @@
+package com.codekul.viewanimations;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onAnimate(View view) {
+        Animation an =
+                AnimationUtils.loadAnimation(
+                        this,
+                        R.anim.rotate
+                );
+
+        findViewById(R.id.imageView).startAnimation(an);
+    }
+}
